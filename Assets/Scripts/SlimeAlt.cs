6691e967d9bb8ba56872e29896cdabffc828684e
Lily <ljim@uoregon.edu>
2020-04-30 14:28:18 -0700

    created new slime prefab

---
+++ Assets/Scripts/SlimeAlt.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SlimeAlt : MonoBehaviour
+{
+    public Text slimeText;
+
+    public void ShowMessage()
+    {
+        slimeText.gameObject.SetActive(true);
+    }
+
+    public void RemoveMessage()
+    {
+        slimeText.gameObject.SetActive(false);
+    }
+}
